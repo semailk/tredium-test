70c1d9668d7f550d7fbb610321556e890ee052d5
Semailk <sem.alik@mail.ru>
2022-05-31 09:51:32 +0300

    fixed

--- database/migrations/2022_05_30_214857_create_articles_table.php
+++ database/migrations/2022_05_30_214857_create_articles_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('description');
             $table->string('image');
-            $table->string('slug')->index();
+            $table->string('slug')->index()->unique();
             $table->timestamps();
         });
     }
